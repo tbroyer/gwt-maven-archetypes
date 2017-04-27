2b01d79764b10f664029539d96976adfe1f61b1a
Thomas Broyer <t.broyer@ltgt.net>
2017-04-27 17:12:31 +0200

    Travis-CI: properly skip tests when deploying snapshots

--- ci/deploy-snapshot.sh
+++ ci/deploy-snapshot.sh
@@ -2,5 +2,5 @@ if [ "$TRAVIS_REPO_SLUG" == "tbroyer/gwt-maven-archetypes" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
 
-  mvn -s ci/settings.xml deploy -DskipTests
+  mvn -s ci/settings.xml deploy -Darchetype.test.skip
 fi
