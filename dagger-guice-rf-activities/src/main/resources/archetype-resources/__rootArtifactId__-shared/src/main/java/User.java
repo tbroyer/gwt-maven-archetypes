e2835682ea9daf66eeb9a880d30210af5de6e669
Thomas Broyer <t.broyer@ltgt.net>
2017-05-05 11:59:17 +0200

    Add missing JsInterop annotations

--- dagger-guice-rf-activities/src/main/resources/archetype-resources/__rootArtifactId__-shared/src/main/java/User.java
+++ dagger-guice-rf-activities/src/main/resources/archetype-resources/__rootArtifactId__-shared/src/main/java/User.java
@@ -1,12 +1,15 @@
 package ${package};
 
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true)
 public interface User {
 
+	@JsProperty
 	String getUserName();
 
+	@JsProperty
 	boolean isAdmin();
 }
 
