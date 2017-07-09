8a9aa7345f65eb1e23e319eed8b84aee638cf754
Thomas Broyer <t.broyer@ltgt.net>
2017-07-09 16:34:34 +0200

    Stop deploying snapshots to Sonatype OSSRH.
    maven-archetype-plugin only uses Central or local repositories
    anyway in latest versions, so deploying snapshots has become
    useless. For users of older Maven versions with an older
    maven-archetype-plugin version, they can still use jitpack.io.

--- ci/deploy-snapshot.sh
+++
@@ -1,6 +0,0 @@
-if [ "$TRAVIS_REPO_SLUG" == "tbroyer/gwt-maven-archetypes" ] && \
-   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
-   [ "$TRAVIS_BRANCH" == "master" ]; then
-
-  mvn -s ci/settings.xml deploy -Darchetype.test.skip
-fi
